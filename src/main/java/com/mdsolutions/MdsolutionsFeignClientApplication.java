e880f1611817b3bbb8a55648b61ec4c47d1f22f2
NAVEEN <boda.gadhilinga@gmail.com>
2022-08-25 16:21:58 +0530

    Merging from Master to Develop

--- src/main/java/com/mdsolutions/MdsolutionsFeignClientApplication.java
+++ src/main/java/com/mdsolutions/MdsolutionsFeignClientApplication.java
@@ -2,14 +2,12 @@ package com.mdsolutions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
-@EnableEurekaClient
-@EnableDiscoveryClient
+
+
 @SpringBootApplication
+@EnableFeignClients
 public class MdsolutionsFeignClientApplication {
 
 	public static void main(String[] args) {
