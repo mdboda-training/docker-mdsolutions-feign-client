b72cc295a3ba497e97f20f5893b73b43021a0aee
MuraliDhar Boda <100523854+mdboda-training@users.noreply.github.com>
2022-11-24 20:25:39 +0530

    Update MdsolutionsFeignClientApplication.java

--- src/main/java/com/mdsolutions/MdsolutionsFeignClientApplication.java
+++ src/main/java/com/mdsolutions/MdsolutionsFeignClientApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class MdsolutionsFeignClientApplication {
 
 	public static void main(String[] args) { 
+		// this line am adding from Browser
 		SpringApplication.run(MdsolutionsFeignClientApplication.class, args);
 	}
 
